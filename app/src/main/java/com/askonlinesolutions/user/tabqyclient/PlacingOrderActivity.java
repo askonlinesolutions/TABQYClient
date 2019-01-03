8dca25ea2c2cdd3156c0fbdc02c2c5946af21425
AskOnlineSolutions <shaiyad@askonlinesolutions.com>
2019-01-03 10:17:23 +0530

    change in akhilesh branch 03/01/2019 10:17 AM

--- app/src/main/java/com/askonlinesolutions/user/tabqyclient/PlacingOrderActivity.java
+++ app/src/main/java/com/askonlinesolutions/user/tabqyclient/PlacingOrderActivity.java
@@ -1,13 +1,44 @@
 package com.askonlinesolutions.user.tabqyclient;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.askonlinesolutions.user.tabqyclient.Commons.Activity.HomeActivity;
+import com.askonlinesolutions.user.tabqyclient.Commons.Activity.SplashActivity;
+import com.askonlinesolutions.user.tabqyclient.Reservation.Activity.Thankyou;
 
 public class PlacingOrderActivity extends AppCompatActivity {
+    private static final long SPLASH_TIME_OUT = 3000;
+    TextView activity_login_title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_placing_order2);
+        activity_login_title = findViewById(R.id.activity_login_title);
+
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+
+                Intent i = new Intent(PlacingOrderActivity.this, Thankyou.class);
+                startActivity(i);
+
+
+            }
+        }, SPLASH_TIME_OUT);
+        activity_login_title.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
     }
 }
