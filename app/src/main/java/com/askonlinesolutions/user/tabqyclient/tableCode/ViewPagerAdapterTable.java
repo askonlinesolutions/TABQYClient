5a24183725e55b477e6eaef2d957e32737fae842
AskOnlineSolutions <shaiyad@askonlinesolutions.com>
2019-01-04 18:46:49 +0530

    change in akhilesh branch 04/01/2019 06:46 PM

--- app/src/main/java/com/askonlinesolutions/user/tabqyclient/tableCode/ViewPagerAdapterTable.java
+++ app/src/main/java/com/askonlinesolutions/user/tabqyclient/tableCode/ViewPagerAdapterTable.java
@@ -11,23 +11,22 @@ import com.askonlinesolutions.user.tabqyclient.OnlineOrder.Fragments.OfferFragme
 import com.askonlinesolutions.user.tabqyclient.OnlineOrder.Fragments.RestroFragment;
 import com.askonlinesolutions.user.tabqyclient.OnlineOrder.Fragments.account.AccountFragment;
 import com.askonlinesolutions.user.tabqyclient.tableCode.menuDashboard.MenuDashboardFragment;
+import com.askonlinesolutions.user.tabqyclient.tableCode.orderStatus.TableOrderStatusFragment;
 import com.askonlinesolutions.user.tabqyclient.tableCode.qrCode.QrCodeFragment;
 
 public class ViewPagerAdapterTable extends FragmentPagerAdapter {
 
     Context context;
     int total_tab = 5;
-    public ViewPagerAdapterTable(FragmentManager fm, Context context)
-    {
+
+    public ViewPagerAdapterTable(FragmentManager fm, Context context) {
         super(fm);
         this.context = context;
     }
 
     @Override
-    public Fragment getItem(int position)
-    {
-        switch (position)
-        {
+    public Fragment getItem(int position) {
+        switch (position) {
             case 0:
                 return new MenuDashboardFragment();
             case 1:
@@ -35,18 +34,17 @@ public class ViewPagerAdapterTable extends FragmentPagerAdapter {
             case 2:
                 return new MyCartFragment();
             case 3:
-                return new OrderStatusFragment();
+                return new TableOrderStatusFragment();
             case 4:
                 return new QrCodeFragment();
             default:
-                return    new MenuDashboardFragment();
+                return new MenuDashboardFragment();
 
         }
     }
 
     @Override
-    public int getCount()
-    {
+    public int getCount() {
         return total_tab;
     }
 
